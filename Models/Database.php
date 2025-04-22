74e3b6fa45a92f1a7a48409b8ea2bf57dd5afe40
adrianspata <adrian.spata@hotmail.com>
2025-04-22 09:19:35 +0200

    update

--- Models/Database.php
+++ Models/Database.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 require_once('Models/UserDatabase.php');
 
@@ -10,118 +10,166 @@ require_once('Models/UserDatabase.php');
 
 
 
-    class Database{
-        public $pdo; // PDO är PHP Data Object - en klass som finns i PHP för att kommunicera med databaser
-        // I $pdo finns nu funktioner (dvs metoder!) som kan användas för att kommunicera med databasen
+class Database
+{
+    public $pdo; // PDO är PHP Data Object - en klass som finns i PHP för att kommunicera med databaser
+    // I $pdo finns nu funktioner (dvs metoder!) som kan användas för att kommunicera med databasen
 
-        private $usersDatabase;
-        function getUsersDatabase(){
-            return $this->usersDatabase;
-        }        
+    private $usersDatabase;
+    function getUsersDatabase()
+    {
+        return $this->usersDatabase;
+    }
 
-        
-        // Note to Stefan STATIC så inte initieras varje gång
-        
-        // SKILJ PÅ CONFIGURATION OCH KOD
 
-        function __construct() {    
-            $host = $_ENV['HOST'];
-            $db   = $_ENV['DB'];
-            $user = $_ENV['USER'];
-            $pass = $_ENV['PASSWORD'];
-            $port = $_ENV['PORT'];
+    // Note to Stefan STATIC så inte initieras varje gång
 
-            $dsn = "mysql:host=$host:$port;dbname=$db"; // connection string
-            $this->pdo = new PDO($dsn, $user, $pass);
-            $this->initDatabase();
-            $this->initData();
-            $this->usersDatabase = new UserDatabase($this->pdo);
-            $this->usersDatabase->setupUsers();
-            $this->usersDatabase->seedUsers();
+    // SKILJ PÅ CONFIGURATION OCH KOD
+
+    function __construct()
+    {
+        $host = $_ENV['HOST'];
+        $db = $_ENV['DB'];
+        $user = $_ENV['USER'];
+        $pass = $_ENV['PASSWORD'];
+        $port = $_ENV['PORT'];
+
+        $dsn = "mysql:host=$host:$port;dbname=$db"; // connection string
+        $this->pdo = new PDO($dsn, $user, $pass);
+        $this->initDatabase();
+        $this->initData();
+        $this->usersDatabase = new UserDatabase($this->pdo);
+        $this->usersDatabase->setupUsers();
+        $this->usersDatabase->seedUsers();
+    }
+
+    function addProductIfNotExists($title, $price, $stockLevel, $categoryName)
+    {
+        $query = $this->pdo->prepare("SELECT * FROM Products WHERE title = :title");
+        $query->execute(['title' => $title]);
+        if ($query->rowCount() == 0) {
+            $this->insertProduct($title, $stockLevel, $price, $categoryName);
         }
-        function initData(){
-            // if select count(*) from Products = 0
-            // lägg till 50 produkter
-            $sql = "SELECT COUNT(*) FROM Products";
-            $res = $this->pdo->query($sql);
-            $count = $res->fetchColumn();
-            if($count == 0){
-                $this->pdo->query("INSERT INTO Products (title, price, stockLevel, categoryName) VALUES ('Banana', 10, 100, 'Fruit')");
-                $this->pdo->query("INSERT INTO Products (title, price, stockLevel, categoryName) VALUES ('Apple', 5, 50, 'Fruit')");
-                $this->pdo->query("INSERT INTO Products (title, price, stockLevel, categoryName) VALUES ('Pear', 7, 70, 'Fruit')");
-                $this->pdo->query("INSERT INTO Products (title, price, stockLevel, categoryName) VALUES ('Cucumber', 15, 30, 'Vegetable')");
-                $this->pdo->query("INSERT INTO Products (title, price, stockLevel, categoryName) VALUES ('Tomato', 20, 40, 'Vegetable')");
-                $this->pdo->query("INSERT INTO Products (title, price, stockLevel, categoryName) VALUES ('Carrot', 10, 20, 'Vegetable')");
-            }
-        }
+    }
 
-        function initDatabase(){
-            $this->pdo->query('CREATE TABLE IF NOT EXISTS Products (
+
+    function initData()
+    {
+        $this->addProductIfNotExists("Banana", 10, 100, "Fruit");
+        $this->addProductIfNotExists("Apple", 5, 50, "Fruit");
+        $this->addProductIfNotExists("Pear", 7, 70, "Fruit");
+        $this->addProductIfNotExists("Cucumber", 15, 30, "Vegetable");
+        $this->addProductIfNotExists("Tomato", 20, 40, "Vegetable");
+        $this->addProductIfNotExists("Carrot", 10, 20, "Vegetable");
+        $this->addProductIfNotExists("Potato", 5, 50, "Vegetable");
+        $this->addProductIfNotExists("Onion", 7, 70, "Vegetable");
+        $this->addProductIfNotExists("Lettuce", 15, 30, "Vegetable");
+        $this->addProductIfNotExists("Broccoli", 20, 40, "Vegetable");
+        $this->addProductIfNotExists("Spinach", 10, 20, "Vegetable");
+        $this->addProductIfNotExists("Zucchini", 5, 50, "Vegetable");
+        $this->addProductIfNotExists("Eggplant", 7, 70, "Vegetable");
+        $this->addProductIfNotExists("Bell Pepper", 15, 30, "Vegetable");
+        $this->addProductIfNotExists("Pinapple", 18, 20, "Fruit");
+    }
+
+    function initDatabase()
+    {
+        $this->pdo->query('CREATE TABLE IF NOT EXISTS Products (
                 id INT AUTO_INCREMENT PRIMARY KEY,
                 title VARCHAR(50),
                 price INT,
                 stockLevel INT,
                 categoryName VARCHAR(50)
             )');
+    }
+
+    function getProduct($id)
+    {
+        $query = $this->pdo->prepare("SELECT * FROM Products WHERE id = :id");
+        $query->execute(['id' => $id]);
+        $query->setFetchMode(PDO::FETCH_CLASS, 'Product');
+        return $query->fetch();
+    }
+
+    function updateProduct($product)
+    {
+        $s = "UPDATE Products SET title = :title," .
+            " price = :price, stockLevel = :stockLevel, categoryName = :categoryName WHERE id = :id";
+        $query = $this->pdo->prepare($s);
+        $query->execute([
+            'title' => $product->title,
+            'price' => $product->price,
+            'stockLevel' => $product->stockLevel,
+            'categoryName' => $product->categoryName,
+            'id' => $product->id
+        ]);
+    }
+
+    function deleteProduct($id)
+    {
+        $query = $this->pdo->prepare("DELETE FROM Products WHERE id = :id");
+        $query->execute(['id' => $id]);
+    }
+
+    function insertProduct($title, $stockLevel, $price, $categoryName)
+    {
+        $sql = "INSERT INTO Products (title, price, stockLevel, categoryName) VALUES (:title, :price, :stockLevel, :categoryName)";
+        $query = $this->pdo->prepare($sql);
+        $query->execute([
+            'title' => $title,
+            'price' => $price,
+            'stockLevel' => $stockLevel,
+            'categoryName' => $categoryName
+        ]);
+    }
+
+
+    function searchProducts($q, $sortCol, $sortOrder)
+    {
+        if (!in_array($sortCol, ["title", "price"])) {
+            $sortCol = "title";
+        }
+        if (!in_array($sortOrder, ["asc", "desc"])) {
+            $sortOrder = "asc";
         }
 
-        function getProduct($id){
-            $query = $this->pdo->prepare("SELECT * FROM Products WHERE id = :id");
-            $query->execute(['id' => $id]);
-            $query->setFetchMode(PDO::FETCH_CLASS, 'Product');
-            return $query->fetch();
+        $query = $this->pdo->prepare("SELECT * FROM Products WHERE title LIKE :q or categoryName like :q ORDER BY $sortCol $sortOrder"); // Products är TABELL
+        $query->execute(['q' => "%$q%"]);
+        return $query->fetchAll(PDO::FETCH_CLASS, 'Product');
+    }
+
+
+    //function getAllProducts($sortCol, $sortOrder){
+    function getAllProducts($sortCol = "id", $sortOrder = "asc")
+    {
+        if (!in_array($sortCol, ["id", "categoryName", "title", "price", "stockLevel"])) {
+            $sortCol = "id";
+        }
+        if (!in_array($sortOrder, ["asc", "desc"])) {
+            $sortOrder = "asc";
         }
 
-        function updateProduct($product){
-            $s = "UPDATE Products SET title = :title," .
-                " price = :price, stockLevel = :stockLevel, categoryName = :categoryName WHERE id = :id";
-            $query = $this->pdo->prepare($s);
-            $query->execute(['title' => $product->title, 'price' => $product->price,
-                'stockLevel' => $product->stockLevel, 'categoryName' => $product->categoryName, 
-                'id' => $product->id]);
-        }
+        // SELECT * FROM Products ORDER BY  id asc
+        $query = $this->pdo->query("SELECT * FROM Products ORDER BY $sortCol $sortOrder"); // Products är TABELL 
+        return $query->fetchAll(PDO::FETCH_CLASS, 'Product'); // Product är PHP Klass
+    }
 
-        function deleteProduct($id){
-            $query = $this->pdo->prepare("DELETE FROM Products WHERE id = :id");
-            $query->execute(['id' => $id]);
-        }
-
-        function insertProduct($title, $stockLevel, $price, $categoryName) {
-            $sql = "INSERT INTO Products (title, price, stockLevel, categoryName) VALUES (:title, :price, :stockLevel, :categoryName)";
-            $query = $this->pdo->prepare($sql);
-            $query->execute(['title' => $title, 'price' => $price,
-                'stockLevel' => $stockLevel, 'categoryName' => $categoryName]);
-        }
-
-
-        //function getAllProducts($sortCol, $sortOrder){
-        function getAllProducts($sortCol="id", $sortOrder= "asc"){
-            if(!in_array($sortCol,["id", "categoryName",  "title","price","stockLevel"])){
-                $sortCol = "id";
-            }
-            if(!in_array($sortOrder,["asc", "desc"])){
-                $sortOrder = "asc";
-            }
-
-            // SELECT * FROM Products ORDER BY  id asc
-            $query = $this->pdo->query("SELECT * FROM Products ORDER BY $sortCol $sortOrder"); // Products är TABELL 
+    function getCategoryProducts($catName)
+    {
+        if ($catName == "") {
+            $query = $this->pdo->query("SELECT * FROM Products"); // Products är TABELL 
             return $query->fetchAll(PDO::FETCH_CLASS, 'Product'); // Product är PHP Klass
         }
-
-        function getCategoryProducts($catName){
-            if($catName == ""){
-                $query = $this->pdo->query("SELECT * FROM Products"); // Products är TABELL 
-                return $query->fetchAll(PDO::FETCH_CLASS, 'Product'); // Product är PHP Klass
-            }
-            $query = $this->pdo->prepare("SELECT * FROM Products WHERE categoryName = :categoryName");
-            $query->execute(['categoryName' => $catName]);
-            return $query->fetchAll(PDO::FETCH_CLASS, 'Product');
-        }
-        function getAllCategories(){
-                // SELECT DISTINCT categoryName FROM Products
-            $data = $this->pdo->query('SELECT DISTINCT categoryName FROM Products')->fetchAll(PDO::FETCH_COLUMN);
-            return $data;
-        }
-
+        $query = $this->pdo->prepare("SELECT * FROM Products WHERE categoryName = :categoryName");
+        $query->execute(['categoryName' => $catName]);
+        return $query->fetchAll(PDO::FETCH_CLASS, 'Product');
     }
+    function getAllCategories()
+    {
+        // SELECT DISTINCT categoryName FROM Products
+        $data = $this->pdo->query('SELECT DISTINCT categoryName FROM Products')->fetchAll(PDO::FETCH_COLUMN);
+        return $data;
+    }
+
+}
 ?>
--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 require_once("Utils/router.php"); // LADDAR IN ROUTER KLASSEN
 require_once("vendor/autoload.php"); // LADDA ALLA DEPENDENCIES FROM VENDOR
 //  :: en STATIC funktion
-$dotenv = Dotenv\Dotenv::createImmutable("."); // . is  current folder for the PAGE
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 // Pilar istf .
 // \ istf .
@@ -19,41 +19,42 @@ $dotenv->load();
 
 $router = new Router();
 $router->addRoute('/', function () {
-    require_once( __DIR__ .'/Pages/index.php');
+    require_once(__DIR__ . '/Pages/index.php');
 });
 $router->addRoute('/category', function () {
-    require_once( __DIR__ .'/Pages/category.php');
+    require_once(__DIR__ . '/Pages/category.php');
 });
 $router->addRoute('/admin/products', function () {
-    require_once( __DIR__ .'/Pages/admin.php' );
+    require_once(__DIR__ . '/Pages/admin.php');
 });
 $router->addRoute('/admin/edit', function () {
-    require_once( __DIR__ .'/Pages/edit.php');
+    require_once(__DIR__ . '/Pages/edit.php');
 });
 $router->addRoute('/admin/new', function () {
-    require_once( __DIR__ .'/Pages/new.php');
+    require_once(__DIR__ . '/Pages/new.php');
 });
 $router->addRoute('/admin/delete', function () {
-    require_once( __DIR__ .'/Pages/delete.php');
+    require_once(__DIR__ . '/Pages/delete.php');
 });
 
 $router->addRoute('/user/login', function () {
-    require_once( __DIR__ .'/Pages/users/login.php');
+    require_once(__DIR__ . '/Pages/users/login.php');
 });
 $router->addRoute('/user/logout', function () {
-    require_once( __DIR__ .'/Pages/users/logout.php');
+    require_once(__DIR__ . '/Pages/users/logout.php');
 });
 
 $router->addRoute('/user/register', function () {
-    require_once( __DIR__ .'/Pages/users/register.php');
+    require_once(__DIR__ . '/Pages/users/register.php');
 });
 
 $router->addRoute('/user/registerThanks', function () {
-    require_once( __DIR__ .'/Pages/users/registerThanks.php');
+    require_once(__DIR__ . '/Pages/users/registerThanks.php');
 });
 
+$router->addRoute('/search', function () {
+    require_once(__DIR__ . '/Pages/search.php');
+});
 
 $router->dispatch();
 ?>
-
-
